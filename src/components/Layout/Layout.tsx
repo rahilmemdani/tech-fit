4bae9626b89b6a9da410bd8e40835ba6fcadab17
rahilmemdani <rmemdanib@gmail.com>
2026-03-07 12:36:14 +0530

    Added Whatsapp button

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import WhatsAppButton from '../WhatsAppButton';
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -14,6 +15,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
                 {children}
             </main>
             <Footer />
+            <WhatsAppButton />
         </div>
     );
 };
