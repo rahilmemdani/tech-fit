4a08d2a7458ce7017daea202736b679333b23c0d
rahilmemdani <websiteadmin-5707>
2026-05-14 13:20:34 +0530

    Removed contact us from nav - duplicate

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar: React.FC = () => {
     { name: 'Services', path: '/services' },
     // { name: 'Technology', path: '/technology' },
     { name: 'Pricing', path: '/pricing' },
-    { name: 'Contact', path: '/contact' },
+    // { name: 'Contact', path: '/contact' },
   ];
 
   useEffect(() => {
